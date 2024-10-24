6e7715648c05f2cf0cccab28d404efd114499f97
Safoora Yousefi <sayouse@microsoft.com>
2024-10-24 17:45:49 +0000

    clean up

--- eureka_ml_insights/data_utils/aime_utils.py
+++ eureka_ml_insights/data_utils/aime_utils.py
@@ -1,7 +1,3 @@
-# Part of this file was authored by authors of the Kitab dataset (https://huggingface.co/datasets/microsoft/kitab)
-# Some code in this file is copied from the original source repository and then adapted to fit this repository.
-# The original license for this code is Community Data License Agreement - Permissive - Version 2.0
-
 import re
 from dataclasses import dataclass
 
@@ -24,11 +20,12 @@ def parse_output_answer(response):
     """
     Parse the input string to extract answer of a given AIME question.
     Parameters:
-    s (str): Input string containing answer X in the form of "Final Answer: X".
-    Returns:
-    dict: A numeric value representing the model's output.
-          Example: 255
+        response (str): Input string containing answer X in the form of "Final Answer: X".
+    Returns: 
+        numerical_value (float): A numeric value representing the model's answer.
     """
+    numerical_value = None
+
     # Try to find an answer in the "Final Answer: X" format
     match = re.search(r"Final Answer:\s*([\$]?-?[\d,]+(?:\.\d+)?%?)", response)
     if match:
@@ -37,12 +34,7 @@ def parse_output_answer(response):
         # If that fails, look for any number with optional $ or % in the response
         match = re.search(r"([\$]?-?[\d,]+(?:\.\d+)?%?)", response)
         answer_str = match.group(1) if match else None
-    # return float(answer_str)
-    numerical_value = None
-    # Store the original format
-    original_format = answer_str
 
-    print("----answer string", answer_str)
     if answer_str:
         # Remove $ and commas, handle percentages for numerical comparison
         answer_str = answer_str.replace("$", "").replace(",", "")
@@ -52,13 +44,6 @@ def parse_output_answer(response):
             try:
                 numerical_value = float(answer_str)
             except ValueError as e:
-                print(f"Error converting answer '{answer_str}' to numerical value: {e}")
                 numerical_value = None
-
-    if numerical_value is not None:
-        print(f"Extracted Answer: {original_format}")
-        print(f"Numerical Value for Comparison: {numerical_value}")
-    else:
-        print("No valid answer extracted.")
 
     return numerical_value
