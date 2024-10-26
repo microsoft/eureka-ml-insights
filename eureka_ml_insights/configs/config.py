7eff14afb57953274365e551e7c5c0102893df39
Safoora Yousefi <sayouse@microsoft.com>
2024-10-26 00:03:41 +0000

    rate limiting + parallelinf

--- eureka_ml_insights/configs/config.py
+++ eureka_ml_insights/configs/config.py
@@ -1,6 +1,6 @@
 import copy
 from dataclasses import dataclass, field
-from typing import Any, Type, TypeVar, List
+from typing import Any, List, Type, TypeVar
 
 UtilityClassConfigType = TypeVar("UtilityClassConfigType", bound=Type["UtilityClassConfig"])
 ComponentConfigType = TypeVar("ComponentConfigType", bound=Type["ComponentConfig"])
@@ -10,11 +10,12 @@ ComponentConfigType = TypeVar("ComponentConfigType", bound=Type["ComponentConfig
 
 @dataclass
 class UtilityClassConfig:
-    """ Base class for all utility class configs 
+    """Base class for all utility class configs
     Args:
         class_name (Any): The utility class to be used with this config
         init_args (dict): The arguments to be passed to the utility class constructor
     """
+
     class_name: Any = None
     init_args: dict = field(default_factory=dict)
 
@@ -52,70 +53,78 @@ class AggregatorConfig(UtilityClassConfig):
 
 @dataclass
 class ComponentConfig:
-    """ Base class for all component configs
+    """Base class for all component configs
     Args:
         component_type (Any): The component class to be used with this config
         output_dir (str): The directory to save the output files of this component
     """
+
     component_type: Any = None
     output_dir: str = None
 
 
 @dataclass
 class DataProcessingConfig(ComponentConfig):
-    """ Config class for the data processing component
+    """Config class for the data processing component
     Args:
         data_reader_config (UtilityClassConfig): The data reader config to be used with this component
         output_data_columns (list): List of columns (subset of input columns) to keep in the transformed data output file
     """
+
     data_reader_config: UtilityClassConfigType = None
     output_data_columns: List[str] = None
 
 
 @dataclass
 class PromptProcessingConfig(DataProcessingConfig):
-    """ Config class for the prompt processing component
+    """Config class for the prompt processing component
     Args:
         prompt_template_path (str): The path to the prompt template jinja file
         ignore_failure (bool): Whether to ignore the failures in the prompt processing and move on
     """
+
     prompt_template_path: str = None
     ignore_failure: bool = False
 
 
 @dataclass
 class DataJoinConfig(DataProcessingConfig):
-    """ Config class for the data join component
+    """Config class for the data join component
     Args:
         other_data_reader_config (UtilityClassConfig): The data reader config for the dataset to be joined with the main dataset
         pandas_merge_args (dict): Arguments to be passed to pandas merge function
     """
+
     other_data_reader_config: UtilityClassConfigType = None
     pandas_merge_args: dict = None
 
 
 @dataclass
 class InferenceConfig(ComponentConfig):
-    """ Config class for the inference component
+    """Config class for the inference component
     Args:
         data_loader_config (UtilityClassConfig): The data loader config to be used with this component
         model_config (UtilityClassConfig): The model config to be used with this component
         resume_from (str): Optional. Path to the file where previous inference results are stored
     """
+
     data_loader_config: UtilityClassConfigType = None
     model_config: UtilityClassConfigType = None
     resume_from: str = None
+    n_calls_per_min: int = None
+    max_concurrent: int = 1
 
 
 @dataclass
 class EvalReportingConfig(ComponentConfig):
-    """ Config class for the evaluation reporting component
+    """Config class for the evaluation reporting component
     Args:
         data_reader_config (UtilityClassConfig): The data reader config to configure the data reader for this component
-        metric_config (UtilityClassConfig): The metric config 
+        metric_config (UtilityClassConfig): The metric config
         aggregator_configs (list): List of aggregator configs
         visualizer_configs (list): List of visualizer configs
     """
+
     data_reader_config: UtilityClassConfigType = None
     metric_config: UtilityClassConfigType = None
     aggregator_configs: List[UtilityClassConfigType] = field(default_factory=list)
@@ -127,11 +136,12 @@ class EvalReportingConfig(ComponentConfig):
 
 @dataclass
 class PipelineConfig:
-    """ Config class for the pipeline class
+    """Config class for the pipeline class
     Args:
         component_configs (list): List of ComponentConfigs
         log_dir (str): The directory to save the logs of the pipeline
     """
+
     component_configs: list[ComponentConfigType] = field(default_factory=list)
     log_dir: str = None
 
--- eureka_ml_insights/core/inference.py
+++ eureka_ml_insights/core/inference.py
@@ -1,5 +1,9 @@
+import asyncio
 import logging
 import os
+import time
+from collections import deque
+from concurrent.futures import ThreadPoolExecutor
 
 from tqdm import tqdm
 
@@ -7,9 +11,11 @@ from eureka_ml_insights.data_utils.data import DataReader, JsonLinesWriter
 
 from .pipeline import Component
 
+MINUTE = 60
+
 
 class Inference(Component):
-    def __init__(self, model_config, data_config, output_dir, resume_from=None):
+    def __init__(self, model_config, data_config, output_dir, resume_from=None, n_calls_per_min=None, max_concurrent=1):
         """
         Initialize the Inference component.
         args:
@@ -17,6 +23,8 @@ class Inference(Component):
             data_config (dict): DataSetConfig object.
             output_dir (str): Directory to save the inference results.
             resume_from (str): optional. Path to the file where previous inference results are stored.
+            n_calls_per_min (int): optional. Number of calls to be made per minute, used for rate limiting. If not provided, rate limiting will not be applied.
+            max_concurrent (int): optional. Maximum number of concurrent inferences to run. Default is 1.
         """
         super().__init__(output_dir)
         self.model = model_config.class_name(**model_config.init_args)
@@ -26,9 +34,21 @@ class Inference(Component):
         if resume_from and not os.path.exists(resume_from):
             raise FileNotFoundError(f"File {resume_from} not found.")
 
+        self.n_calls_per_min = n_calls_per_min
+        self.max_concurrent = max_concurrent
+        self.call_times = deque()
+        self.period = MINUTE
+
     @classmethod
     def from_config(cls, config):
-        return cls(config.model_config, config.data_loader_config, config.output_dir, config.resume_from)
+        return cls(
+            config.model_config,
+            config.data_loader_config,
+            config.output_dir,
+            resume_from=config.resume_from,
+            n_calls_per_min=config.n_calls_per_min,
+            max_concurrent=config.max_concurrent,
+        )
 
     def fetch_previous_inference_results(self):
         # fetch previous results from the provided resume_from file
@@ -58,28 +78,96 @@ class Inference(Component):
         logging.info(f"Last uid inferenced: {last_uid}")
         return pre_inf_results_df, last_uid
 
+    def validate_response_dict(self, response_dict):
+        # "model_output" and "is_valid" are mandatory fields by any inference component
+        if "model_output" not in response_dict or "is_valid" not in response_dict:
+            raise ValueError("Response dictionary must contain 'model_output' and 'is_valid' keys.")
+
+    def retrieve_exisiting_result(self, data, pre_inf_results_df):
+        # if resume_from file is provided and valid inference results
+        # for the current data point are present in it, use them.
+        prev_results = pre_inf_results_df[pre_inf_results_df.uid == data["uid"]]
+        prev_result_is_valid = bool(prev_results["is_valid"].values[0])
+        prev_model_output = prev_results["model_output"].values[0]
+        if prev_result_is_valid:
+            logging.info(f"Skipping inference for uid: {data['uid']}. Using previous results.")
+            data["model_output"], data["is_valid"] = prev_model_output, prev_result_is_valid
+            return data
+
     def run(self):
+        if self.max_concurrent > 1:
+            asyncio.run(self._run_par())
+        else:
+            self._run()
+
+    def _run(self):
         if self.resume_from:
             pre_inf_results_df, last_uid = self.fetch_previous_inference_results()
         with self.data_loader as loader:
             with self.writer as writer:
                 for data, model_inputs in tqdm(loader, desc="Inference Progress:"):
-                    # if resume_from file is provided and valid inference results
-                    # for the current data point are present in it, use them.
+
                     if self.resume_from and (data["uid"] <= last_uid):
-                        prev_results = pre_inf_results_df[pre_inf_results_df.uid == data["uid"]]
-                        prev_result_is_valid = bool(prev_results["is_valid"].values[0])
-                        prev_model_output = prev_results["model_output"].values[0]
-                        if prev_result_is_valid:
-                            logging.info(f"Skipping inference for uid: {data['uid']}. Using previous results.")
-                            data["model_output"], data["is_valid"] = prev_model_output, prev_result_is_valid
-                            writer.write(data)
+                        prev_result = self.retrieve_exisiting_result(data, pre_inf_results_df)
+                        if prev_result:
+                            writer.write(prev_result)
                             continue
-                    # generate text from model
+
+                    # generate text from model (optionally at a limited rate)
+                    if self.n_calls_per_min:
+                        while len(self.call_times) >= self.n_calls_per_min:
+                            # remove the oldest call time if it is older than the rate limit period
+                            if time.time() - self.call_times[0] > self.period:
+                                self.call_times.popleft()
+                            else:
+                                # rate limit is reached, wait for a second
+                                time.sleep(1)
+                        self.call_times.append(time.time())
                     response_dict = self.model.generate(*model_inputs)
-                    # "model_output" and "is_valid" are mandatory fields by any inference component
-                    if "model_output" not in response_dict or "is_valid" not in response_dict:
-                        raise ValueError("Response dictionary must contain 'model_output' and 'is_valid' keys.")
+                    self.validate_response_dict(response_dict)
                     # write results
                     data.update(response_dict)
                     writer.write(data)
+
+    async def run_in_excutor(self, model_inputs, executor):
+        loop = asyncio.get_event_loop()
+        return await loop.run_in_executor(executor, self.model.generate, *model_inputs)
+
+    async def _run_par(self):
+        concurrent_inputs = []
+        concurrent_metadata = []
+        if self.resume_from:
+            pre_inf_results_df, last_uid = self.fetch_previous_inference_results()
+        with self.data_loader as loader:
+            with self.writer as writer:
+                for data, model_inputs in tqdm(loader, desc="Inference Progress:"):
+                    if self.resume_from and (data["uid"] <= last_uid):
+                        prev_result = self.retrieve_exisiting_result(data, pre_inf_results_df)
+                        if prev_result:
+                            writer.write(prev_result)
+                            continue
+
+                    # if batch is ready for concurrent inference
+                    elif len(concurrent_inputs) >= self.max_concurrent:
+                        with ThreadPoolExecutor() as executor:
+                            await self.run_batch(concurrent_inputs, concurrent_metadata, writer, executor)
+                        concurrent_inputs = []
+                        concurrent_metadata = []
+                    else:
+                        # add data to batch for concurrent inference
+                        concurrent_inputs.append(model_inputs)
+                        concurrent_metadata.append(data)
+                # if data loader is exhausted but there are remaining data points that did not form a full batch
+                if concurrent_inputs:
+                    with ThreadPoolExecutor() as executor:
+                        await self.run_batch(concurrent_inputs, concurrent_metadata, writer, executor)
+
+    async def run_batch(self, concurrent_inputs, concurrent_metadata, writer, executor):
+        tasks = [asyncio.create_task(self.run_in_excutor(input_data, executor)) for input_data in concurrent_inputs]
+        results = await asyncio.gather(*tasks)
+        for i in range(len(concurrent_inputs)):
+            data, response_dict = concurrent_metadata[i], results[i]
+            self.validate_response_dict(response_dict)
+            # prepare results for writing
+            data.update(response_dict)
+            writer.write(data)
