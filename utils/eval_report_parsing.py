07fa792f250c307a0e08b81cb3aee7fe91bad9c1
James Woffinden-Luey <jluey@microsoft.com>
2024-11-07 16:00:09 -0800

    adding ability for subcategories

--- utils/eval_report_parsing.py
+++ utils/eval_report_parsing.py
@@ -88,64 +88,72 @@ def create_benchmark_breakdown(release_directory_path, config):
     for benchmark in mapping:
         name = benchmark["name"]
         data[name] = {"graphs": []}
-        description = benchmark["description"]
         file_pattern = re.compile(benchmark["filePattern"], re.IGNORECASE)
-        
-        for graph in benchmark["graphs"]:
-            path = graph["path"]
-            model_families = os.listdir(os.path.join(release_directory_path, *path))
-            model_scores = []
-            for model_family in model_families:
-                if model_family.lower() not in model_family_list:
-                    continue
-                models = os.listdir(os.path.join(release_directory_path, *path, model_family))
-                for model in models:
-                    runs = os.listdir(os.path.join(release_directory_path, *path, model_family, model))
-                    
-                    sum = 0.0
-                    num = 0 # there's a chance that one of the runs doesn't have the correct output file so need to keep track separately
-                    for run in runs:
-                        try:
-                            # if name == "Long Context QA Longest Context (3K)":
-                            #     file_pattern = re.compile(r'^.*by_ctx_size_normalized.*\.json$', re.IGNORECASE)
-                            report = [f for f in os.listdir(os.path.join(release_directory_path, *path, model_family, model, run, 'eval_report')) if file_pattern.match(f)]
-                            if len(report) == 0:
+        for experiment in benchmark["experiments"]:
+            for graph in experiment["series"]:
+                path = graph["path"]
+                model_families = os.listdir(os.path.join(release_directory_path, *path))
+                model_scores = []
+                for model_family in model_families:
+                    if model_family.lower() not in model_family_list:
+                        continue
+                    models = os.listdir(os.path.join(release_directory_path, *path, model_family))
+                    for model in models:
+                        runs = os.listdir(os.path.join(release_directory_path, *path, model_family, model))
+                        
+                        sum = 0.0
+                        num = 0 # there's a chance that one of the runs doesn't have the correct output file so need to keep track separately
+                        for run in runs:
+                            try:
+                                # if name == "Long Context QA Longest Context (3K)":
+                                #     file_pattern = re.compile(r'^.*by_ctx_size_normalized.*\.json$', re.IGNORECASE)
+                                report = [f for f in os.listdir(os.path.join(release_directory_path, *path, model_family, model, run, 'eval_report')) if file_pattern.match(f)]
+                                if len(report) == 0:
+                                    continue
+                                else:
+                                    report = report[0]
+                                file_path = os.path.join(release_directory_path, *path, model_family, model, run, 'eval_report', report)
+                                with open(file_path, 'r') as f:
+                                    file_contents = f.read()
+                                    scores = json.loads(file_contents)
+                                    for metric in graph["metric"]:
+                                        scores = scores[metric]
+                                    for category in scores:
+                                        if category not in model_scores[model]:
+                                            model_scores[model][category] = []
+                                        if type(scores[category]) == float:
+                                            score = scores[category]
+                                        else:
+                                            if name == "Geometric Reasoning": # geometric reasoning reports count instead of percentage
+                                                none = 0
+                                                if "none" in scores and (scores["none"] == scores["none"]): # check for NaN
+                                                    none = scores["none"]
+                                                score = scores["correct"] * 1.0 / (scores["correct"] + scores["incorrect"] + none)
+                                            else:
+                                                score = scores[category]["correct"]
+                                        model_scores[model][category].append(score)
+                                    sum += scores
+                                num += 1
+                                break
+                            except FileNotFoundError:
                                 continue
-                            else:
-                                report = report[0]
-                            file_path = os.path.join(release_directory_path, *path, model_family, model, run, 'eval_report', report)
-                            with open(file_path, 'r') as f:
-                                file_contents = f.read()
-                                scores = json.loads(file_contents)
-                                for metric in graph["metric"]:
-                                    scores = scores[metric]
-                                if name == "Geometric Reasoning": # geometric reasoning reports count instead of percentage
-                                    none = 0
-                                    if "none" in scores and (scores["none"] == scores["none"]):
-                                        none = scores["none"]
-                                    scores = scores["correct"] * 1.0 / (scores["correct"] + scores["incorrect"] + none)
-                                sum += scores
-                            num += 1
-                            break
-                        except FileNotFoundError:
-                            continue
-                    if model == 'GPT-4o_2024_05_13_450K':
-                        model = 'GPT-4o-2024-05-13'
-                    if model == 'GPT-4o_2024_05_13':
-                        model = 'GPT-4o-2024-05-13'
-                    if model == "LLaVA-34B":
-                        model = "Llava-1_6-34B"
-                    if model == "GPT-4":
-                        model = "GPT-4-1106-Preview"
-                    
-                    model_scores.append({   
-                        "name": model,
-                        "score": round(sum  * 100.0 / num, 1)
-                    })
-            data[name]["graphs"].append({
-                "title": graph["title"],
-                "models": model_scores
-            })
+                        if model == 'GPT-4o_2024_05_13_450K':
+                            model = 'GPT-4o-2024-05-13'
+                        if model == 'GPT-4o_2024_05_13':
+                            model = 'GPT-4o-2024-05-13'
+                        if model == "LLaVA-34B":
+                            model = "Llava-1_6-34B"
+                        if model == "GPT-4":
+                            model = "GPT-4-1106-Preview"
+                        
+                        model_scores.append({   
+                            "name": model,
+                            "score": round(sum  * 100.0 / num, 1)
+                        })
+                data[name]["graphs"].append({
+                    "title": graph["title"],
+                    "models": model_scores
+                })
     with open('website/static/benchmark_results.json', 'w') as f:
         json.dump(data, f, indent=2)
 
