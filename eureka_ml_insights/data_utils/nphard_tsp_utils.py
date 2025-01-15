a71db9e2ad2f1c5a2b809a0970d5dfbd06aef348
Vibhav Vineet <vibhav.vineet@gmail.com>
2025-01-14 16:54:20 -0800

    updated nphard tsp task

--- eureka_ml_insights/data_utils/nphard_tsp_utils.py
+++ eureka_ml_insights/data_utils/nphard_tsp_utils.py
@@ -17,12 +17,35 @@ class NPHARDTSPExtractAnswer(DFTransformBase):
         df[self.model_answer_column] = df[self.model_output_column].apply(parse_xml_to_dict)
         return df
 
+def convert_city_path(path):
+    """
+    Convert a path in the format 'City1->City6->City7->...' into '1->6->7->...'.
+
+    Args:
+        path (str): The input path in the format 'CityX->CityY->...'.
+
+    Returns:
+        str: The converted path in numerical format.
+    """
+    # Split the input path into individual cities
+    city_list = path.split("->")
+
+    # Extract the numeric part from each city and join them with '->'
+    numeric_path = "->".join(city.replace("City", "") for city in city_list)
+
+    return numeric_path
+
 def parse_xml_to_dict(xml_string):
     # Parse the XML string
+    # breakpoint()
     root = ET.fromstring(xml_string)
     final_answer_element = root.find('final_answer')
 
     tour_string = ast.literal_eval(final_answer_element.text)['Path']
+
+    if 'City' in tour_string:
+        tour_string = convert_city_path(tour_string)
+
     tour = list(map(int, tour_string.split('->')))
 
     tour_string = ','.join(map(str, tour))
--- eureka_ml_insights/user_configs/nphard_tsp.py
+++ eureka_ml_insights/user_configs/nphard_tsp.py
@@ -42,7 +42,7 @@ class NPHARD_TSP_PIPELINE(ExperimentConfig):
             data_reader_config=DataSetConfig(
                 HFDataReader,
                 {
-                    "path": "GeoMeterData/nphard_tsp2",
+                    "path": "GeoMeterData/nphard_tsp1",
                     "split": "train",
                     "transform": SequenceTransform(
                         [
@@ -64,6 +64,7 @@ class NPHARD_TSP_PIPELINE(ExperimentConfig):
             ),
             output_dir=os.path.join(self.log_dir, "inference_result"),            
             resume_from=resume_from,
+            # resume_from="/home/vivineet/projects/evaluation/NPHardEval/TSP/eureka-ml-insights/logs/NPHARD_TSP_PIPELINE2Run/nphard_tsp_level_03_small/2025-01-14-12-29-12.577458/inference_result/inference_result.jsonl", #resume_from,
             max_concurrent=1,
         )
 
@@ -182,7 +183,7 @@ class NPHARD_TSP_PIPELINE2Run(NPHARD_TSP_PIPELINE):
         pipeline = super().configure_pipeline(model_config=model_config, resume_from=resume_from)
         # data preprocessing
         self.data_processing_comp.data_reader_config.init_args["transform"].transforms.append(
-            MultiplyTransform(n_repeats=3)
+            MultiplyTransform(n_repeats=1)
         )
         return pipeline
     
