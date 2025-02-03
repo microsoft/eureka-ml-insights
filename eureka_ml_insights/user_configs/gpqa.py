bf005ecb7c6d86418e3356d36596e5ff7b5d89c9
Besmira Nushi <benushi@microsoft.com>
2025-02-03 13:23:00 -0800

    llm based answer extraction

--- eureka_ml_insights/user_configs/gpqa.py
+++ eureka_ml_insights/user_configs/gpqa.py
@@ -1,7 +1,13 @@
 import os
 from typing import Any
 
-from eureka_ml_insights.core import EvalReporting, Inference, PromptProcessing, DataProcessing
+from eureka_ml_insights.core import (
+    EvalReporting, 
+    Inference, 
+    PromptProcessing, 
+    DataProcessing,
+    DataJoin
+)
 from eureka_ml_insights.data_utils import (
     ColumnMatchMapTransform,
     CopyColumn,
@@ -16,9 +22,10 @@ from eureka_ml_insights.data_utils import (
     SamplerTransform,
     MajorityVoteTransform,
     ColumnRename,
+    AddColumn,
     ReplaceStringsTransform,
     RunPythonTransform,
-    ExtractUsageTransform
+    ExtractUsageTransform,
 )
 from eureka_ml_insights.metrics import (
     CountAggregator, 
@@ -37,8 +44,10 @@ from eureka_ml_insights.configs import(
     PipelineConfig,
     PromptProcessingConfig,
     DataProcessingConfig,
+    DataJoinConfig
 )
 from eureka_ml_insights.configs import ExperimentConfig
+from eureka_ml_insights.configs.model_configs import TRAPI_GPT4O_2024_11_20_CONFIG, CLAUDE_3_5_SONNET_20241022_CONFIG
 import numpy as np
 
 """This file contains user defined configuration classes for the GPQA dataset.
@@ -94,9 +103,8 @@ class GPQA_Experiment_Pipeline(ExperimentConfig):
             resume_from=resume_from,
             max_concurrent=1
         )
-        # Configure the evaluation and reporting component for pass@1.
-        self.evalreporting_comp = EvalReportingConfig(
-            component_type=EvalReporting,
+        self.preeval_data_post_processing_comp = DataProcessingConfig(
+            component_type=DataProcessing,
             data_reader_config=DataSetConfig(
                 DataReader,
                 {
@@ -117,6 +125,82 @@ class GPQA_Experiment_Pipeline(ExperimentConfig):
                                 case=True,
                             ),
                             ImputeNA(columns="model_output", value="")
+                        ]
+                    ),
+                },
+            ),
+            output_dir=os.path.join(self.log_dir, "preeval_data_post_processing_output"),
+        )
+        self.filter_empty_answer = PromptProcessingConfig(
+            component_type=PromptProcessing,
+            data_reader_config=DataSetConfig(
+                DataReader,
+                {
+                    "path": os.path.join(self.preeval_data_post_processing_comp.output_dir, "transformed_data.jsonl"),
+                    "format": ".jsonl",
+                    "transform": SequenceTransform(
+                        [
+                            RunPythonTransform("df = df[df['model_output'] == '']"),
+                            ColumnRename(name_mapping={"prompt": "initial_prompt"}),
+                            AddColumn(column_name="prompt")
+                        ]
+                    ),
+                },
+            ),
+            prompt_template_path=os.path.join(
+                os.path.dirname(__file__),
+                "../prompt_templates/gpqa_templates/extract_gpqa_answer.jinja",
+            ),
+            output_dir=os.path.join(self.log_dir, "filter_empty_answer"),
+        )
+        self.inference_llm_answer_extract = InferenceConfig(
+            component_type=Inference,
+            model_config=CLAUDE_3_5_SONNET_20241022_CONFIG, #TRAPI_GPT4O_2024_11_20_CONFIG,
+            data_loader_config=DataSetConfig(
+                MMDataLoader,
+                {"path": os.path.join(self.filter_empty_answer.output_dir, "transformed_data.jsonl")},
+            ),
+            output_dir=os.path.join(self.log_dir, "llm_answer_extract_inference_result"),
+            max_concurrent=1
+        )
+        self.data_join = DataJoinConfig(
+            component_type=DataJoin,
+            data_reader_config=DataSetConfig(
+                DataReader,
+                {
+                    "path": os.path.join(self.preeval_data_post_processing_comp.output_dir, "transformed_data.jsonl"),
+                    "format": ".jsonl",
+                },
+            ),
+            other_data_reader_config=DataSetConfig(
+                DataReader,
+                {
+                    "path": os.path.join(self.inference_llm_answer_extract.output_dir, "inference_result.jsonl"),
+                    "format": ".jsonl",
+                    "transform": SequenceTransform(
+                        [
+                            # drop all columns except the uid and model_output
+                            RunPythonTransform("df = df[['uid', 'model_output']]"),
+                            ColumnRename(name_mapping={"model_output": "answer_extract_model_output"}),
+                        ]
+                    ),
+                },
+            ),
+            output_dir=os.path.join(self.log_dir, "data_join_output"),
+            pandas_merge_args={"on": "uid", "how": "left"},
+        )
+        # Configure the evaluation and reporting component for pass@1.
+        self.evalreporting_comp = EvalReportingConfig(
+            component_type=EvalReporting,
+            data_reader_config=DataSetConfig(
+                DataReader,
+                {
+                    "path": os.path.join(self.data_join.output_dir, "transformed_data.jsonl"),
+                    "format": ".jsonl",
+                    "transform": SequenceTransform(
+                        [
+                            # consolidate model_output to replace model_output with answer_extract_model_output whenever empty
+                            RunPythonTransform("df['model_output'] = df.apply(lambda row: row['answer_extract_model_output'] if row['model_output'] == "" else row['model_output'], axis=1)"),
                         ]
                     ),
                 },
@@ -220,7 +304,7 @@ class GPQA_Experiment_Pipeline(ExperimentConfig):
                     ),
                 },
             ),
-            output_dir=os.path.join(self.log_dir, "data_post_processing_output"),
+            output_dir=os.path.join(self.log_dir, "posteval_data_post_processing_output"),
         )
 
         # Aggregate the results by best of n
@@ -353,11 +437,15 @@ class GPQA_Experiment_Pipeline(ExperimentConfig):
             [
                 self.data_processing_comp,
                 self.inference_comp,
-                self.evalreporting_comp,
-                self.posteval_data_post_processing_comp,
-                self.bon_evalreporting_comp,
-                self.data_post_processing_mv,
-                self.mv_evalreporting_comp
+                self.preeval_data_post_processing_comp,
+                self.filter_empty_answer,
+                self.inference_llm_answer_extract,
+                self.data_join,
+                # self.evalreporting_comp,
+                # self.posteval_data_post_processing_comp,
+                # self.bon_evalreporting_comp,
+                # self.data_post_processing_mv,
+                # self.mv_evalreporting_comp
             ],
             self.log_dir,
         )
