e04199f57ac787d0c41fc0ddf77f65557003a5c0
t-uenorisa_microsoft <t-uenorisa@microsoft.com>
2025-02-10 19:49:18 +0000

    Add initial files for GSM8K benchmark

---
+++ eureka_ml_insights/data_utils/gsm8k_utils.py
@@ -0,0 +1,49 @@
+import re
+from dataclasses import dataclass
+
+import pandas as pd
+
+from .transform import DFTransformBase
+
+
+@dataclass
+class GSM8KExtractAnswer(DFTransformBase):
+    model_output_column: str
+    model_answer_column: str
+
+    def transform(self, df: pd.DataFrame) -> pd.DataFrame:
+        df[self.model_answer_column] = df[self.model_output_column].apply(self.parse_output_answer)
+        return df
+
+
+    def parse_output_answer(response):
+        """
+        Parse the input string to extract answer of a given GSM8K question.
+        Parameters:
+            response (str): Input string containing answer X
+        Returns: 
+            numerical_value (float): A numeric value representing the model's answer.
+        """
+        
+        def str_to_float(hyp):
+            for remove_char in [',', '$', '%', 'g']:
+                hyp = hyp.replace(remove_char, '')
+            if hyp == 'True' or hyp == 'true':
+                hyp = True
+            if hyp == 'False' or hyp == 'false':
+                hyp = False
+            try:
+                hyp = float(hyp)
+            except:
+                try:
+                    hyp = eval(hyp) # execute equations
+                    if not ((type(hyp) is int) or (type(hyp) is float)):
+                        hyp = None
+                except:
+                    hyp = None
+            return hyp
+
+        answer = response.split('####')[-1].strip()
+        numerical_value = str_to_float(answer)
+
+        return numerical_value
---
+++ eureka_ml_insights/user_configs/gsm8k.py
@@ -0,0 +1,214 @@
+import os
+from typing import Any
+
+from eureka_ml_insights.configs import (
+    AggregatorConfig,
+    DataProcessingConfig,
+    DataSetConfig,
+    EvalReportingConfig,
+    ExperimentConfig,
+    InferenceConfig,
+    MetricConfig,
+    ModelConfig,
+    PipelineConfig,
+    PromptProcessingConfig,
+)
+from eureka_ml_insights.core import DataProcessing, Inference, PromptProcessing
+from eureka_ml_insights.core.eval_reporting import EvalReporting
+from eureka_ml_insights.data_utils import (
+    AddColumn,
+    ColumnRename,
+    DataReader,
+    HFDataReader,
+    MajorityVoteTransform,
+    MultiplyTransform,
+    SequenceTransform,
+)
+from eureka_ml_insights.data_utils.gsm8k_utils import GSM8KExtractAnswer
+from eureka_ml_insights.data_utils.data import DataLoader
+from eureka_ml_insights.metrics.metrics_base import ExactMatch
+from eureka_ml_insights.metrics.reports import (
+    BiLevelCountAggregator,
+    CountAggregator,
+)
+
+
+class GSM8K_PIPELINE(ExperimentConfig):
+    """This class specifies the config for running GSM8K benchmark on any model"""
+
+    def configure_pipeline(
+        self, model_config: ModelConfig, resume_from: str = None, **kwargs: dict[str, Any]
+    ) -> PipelineConfig:
+
+        # data preprocessing
+        self.data_processing_comp = PromptProcessingConfig(
+            component_type=PromptProcessing,
+            data_reader_config=DataSetConfig(
+                HFDataReader,
+                {
+                    "path": "openai/gsm8k",
+                    "split": "train",
+                    "transform": SequenceTransform(
+                        [
+                            ColumnRename(
+                                name_mapping={
+                                    "Question": "prompt",
+                                    "Answer": "ground_truth",
+                                }
+                            ),
+                        ],
+                    ),
+                },
+            ),
+            prompt_template_path=os.path.join(
+                os.path.dirname(__file__), "../prompt_templates/gsm8k_templates/zeroshot.jinja"
+            ),
+            output_dir=os.path.join(self.log_dir, "data_processing_output"),
+        )
+
+        # inference component
+        self.inference_comp = InferenceConfig(
+            component_type=Inference,
+            model_config=model_config,
+            data_loader_config=DataSetConfig(
+                DataLoader,
+                {"path": os.path.join(self.data_processing_comp.output_dir, "transformed_data.jsonl")},
+            ),
+            output_dir=os.path.join(self.log_dir, "inference_result"),
+            resume_from=resume_from,
+            max_concurrent=10,
+        )
+        # post process the response to extract the answer
+        self.data_post_processing = DataProcessingConfig(
+            component_type=DataProcessing,
+            data_reader_config=DataSetConfig(
+                DataReader,
+                {
+                    "path": os.path.join(self.inference_comp.output_dir, "inference_result.jsonl"),
+                    "format": ".jsonl",
+                    "transform": SequenceTransform(
+                        [
+                            ColumnRename(
+                                name_mapping={
+                                    "model_output": "raw_output",
+                                }
+                            ),
+                            AddColumn("model_output"),
+                            GSM8KExtractAnswer("raw_output", "model_output"),
+                        ]
+                    ),
+                },
+            ),
+            output_dir=os.path.join(self.log_dir, "data_post_processing_output"),
+        )
+
+        # Configure the evaluation and reporting component for evaluation and dataset level aggregation
+        self.evalreporting_comp = EvalReportingConfig(
+            component_type=EvalReporting,
+            data_reader_config=DataSetConfig(
+                DataReader,
+                {
+                    "path": os.path.join(self.data_post_processing.output_dir, "transformed_data.jsonl"),
+                    "format": ".jsonl",
+                },
+            ),
+            metric_config=MetricConfig(ExactMatch),
+            aggregator_configs=[
+                AggregatorConfig(
+                    CountAggregator,
+                    {
+                        "column_names": [
+                            "ExactMatch_result",
+                        ],
+                        "group_by": "Year",
+                        "filename_base": "ExactMatch_GroupBy",
+                    },
+                ),
+            ],
+            output_dir=os.path.join(self.log_dir, "eval_report"),
+        )
+
+        # Aggregate the results by a majority vote
+        # First, let us perform majority_vote
+        self.data_post_processing_addmv = DataProcessingConfig(
+            component_type=DataProcessing,
+            data_reader_config=DataSetConfig(
+                DataReader,
+                {
+                    "path": os.path.join(self.inference_comp.output_dir, "inference_result.jsonl"),
+                    "format": ".jsonl",
+                    "transform": SequenceTransform(
+                        [
+                            ColumnRename(
+                                name_mapping={
+                                    "model_output": "raw_output",
+                                }
+                            ),
+                            AddColumn("model_output"),
+                            GSM8KExtractAnswer("raw_output", "model_output"),
+                            MajorityVoteTransform(id_col="ID"),
+                            ColumnRename(
+                                name_mapping={
+                                    "model_output": "model_output_onerun",
+                                    "majority_vote": "model_output",
+                                }
+                            ),
+                        ]
+                    ),
+                },
+            ),
+            output_dir=os.path.join(self.log_dir, "data_addmv_output"),
+        )
+        # Second, compute eaxct match
+        self.postevalprocess_comp = EvalReportingConfig(
+            component_type=EvalReporting,
+            data_reader_config=DataSetConfig(
+                DataReader,
+                {
+                    "path": os.path.join(self.data_post_processing_addmv.output_dir, "transformed_data.jsonl"),
+                    "format": ".jsonl",
+                },
+            ),
+            metric_config=MetricConfig(ExactMatch),
+            aggregator_configs=[
+                AggregatorConfig(
+                    BiLevelCountAggregator,
+                    {
+                        "column_names": [
+                            "ExactMatch_result",
+                        ],
+                        "first_groupby": "ID",
+                        "filename_base": "MajorityVote",
+                        "normalize": True,
+                    },
+                ),
+            ],
+            output_dir=os.path.join(self.log_dir, "eval_report_majorityVote"),
+        )
+
+        # Configure the pipeline
+        return PipelineConfig(
+            [
+                self.data_processing_comp,
+                self.inference_comp,
+                self.data_post_processing,
+                self.evalreporting_comp,
+                self.data_post_processing_addmv,
+                self.postevalprocess_comp,
+            ],
+            self.log_dir,
+        )
+
+
+class GSM8K_PIPELINE5Run(GSM8K_PIPELINE):
+    """This class specifies the config for running GSM8K benchmark 5 repeated times"""
+
+    def configure_pipeline(
+        self, model_config: ModelConfig, resume_from: str = None, **kwargs: dict[str, Any]
+    ) -> PipelineConfig:
+        pipeline = super().configure_pipeline(model_config=model_config, resume_from=resume_from)
+        # data preprocessing
+        self.data_processing_comp.data_reader_config.init_args["transform"].transforms.append(
+            MultiplyTransform(n_repeats=5)
+        )
+        return pipeline
