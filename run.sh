945d2ea361e97afd7e5e49d86a87c721cc09da3c
Vibhav Vineet <vibhav.vineet@gmail.com>
2025-02-11 10:45:00 -0800

    adding run file

---
+++ run.sh
@@ -0,0 +1,65 @@
+## source .eurekaenv/bin/activate
+
+
+
+# python main.py --exp_config NPHARD_TSP_PIPELINE_Runs --model_config OAI_O1_PREVIEW_CONFIG --exp_logdir nphard_tsp_level_test
+
+# python main.py --exp_config NPHARD_TSP_PIPELINE_Runs --model_config OAI_GPT4O_2024_05_13_CONFIG --exp_logdir nphard_tsp_level_test
+
+## TRAPI model
+
+# python main.py --exp_config NPHARD_TSP_PIPELINE_multipleRuns --model_config TRAPI_GPT4O_2024_05_13_CONFIG --exp_logdir nphard_tsp_level_test
+
+# python main.py --exp_config NPHARD_TSP_PIPELINE_MULTIPLE_RUNS --model_config GEMINI_EXP_1206_CONFIG --exp_logdir nphard_sat_level_gemini-exp-1206 --resume_from /home/vivineet/projects/evaluation/NPHardEval/TSP_02-07-2025/eureka-ml-insights/logs/NPHARD_TSP_PIPELINE_MULTIPLE_RUNS/nphard_sat_level_gemini-exp-1206/2025-02-08-18-58-55.006713/inference_result/inference_result.jsonl
+
+# ### claude
+# python main.py --exp_config NPHARD_TSP_PIPELINE_MULTIPLE_RUNS --model_config CLAUDE_3_5_SONNET_CONFIG --exp_logdir nphard_tsp_level_claude-3-5-sonnet-20241022 --resume_from /home/vivineet/projects/evaluation/NPHardEval/TSP_02-07-2025/eureka-ml-insights/logs/NPHARD_TSP_PIPELINE_MULTIPLE_RUNS/nphard_tsp_level_claude-3-5-sonnet-20241022/2025-02-01-23-25-15.059378/inference_result/inference_result.jsonl
+
+# ### gemini
+# python main.py --exp_config NPHARD_TSP_PIPELINE_MULTIPLE_RUNS --model_config GEMINI_EXP_1206_CONFIG --exp_logdir nphard_tsp_level_gemini-exp-1206 --resume_from /home/vivineet/projects/evaluation/NPHardEval/TSP_02-07-2025/eureka-ml-insights/logs/NPHARD_TSP_PIPELINE_MULTIPLE_RUNS/nphard_tsp_level_gemini-exp-1206/2025-02-08-19-14-48.091758/inference_result/inference_result.jsonl
+
+### gpt4o
+
+# python main.py --exp_config NPHARD_TSP_PIPELINE_MULTIPLE_RUNS --model_config OAI_GPT4O_2024_11_20_CONFIG --exp_logdir nphard_tsp_level_gpt4o_2024-11-20 --resume_from /home/vivineet/projects/evaluation/NPHardEval/TSP_02-07-2025/eureka-ml-insights/logs/NPHARD_TSP_PIPELINE_MULTIPLE_RUNS/nphard_tsp_level_gpt4o_2024-11-20/2025-02-01-23-25-46.681454/inference_result/inference_result.jsonl
+
+# # ### llama_3_1
+# python main.py --exp_config NPHARD_TSP_PIPELINE_MULTIPLE_RUNS --model_config AIF_NT_LLAMA3_1_405B_INSTRUCT_EASTUS_OSS_CONFIG_2 --exp_logdir nphard_tsp_level_llama_3_1_2024-11-20 --resume_from /home/vivineet/projects/evaluation/NPHardEval/TSP_02-07-2025/eureka-ml-insights/logs/NPHARD_TSP_PIPELINE_MULTIPLE_RUNS/nphard_tsp_level_llama_3_1_2024-11-20/2025-02-01-23-28-15.277023/inference_result/inference_result.jsonl
+
+# # # ### phi4 ### not ready
+# python main.py --exp_config NPHARD_TSP_PIPELINE_MULTIPLE_RUNS --model_config PHI4HF_CONFIG --exp_logdir nphard_tsp_level_phi4 --resume_from /home/vivineet/projects/evaluation/NPHardEval/TSP_02-07-2025/eureka-ml-insights/logs/NPHARD_TSP_PIPELINE_MULTIPLE_RUNS/nphard_tsp_level_phi4/2025-02-08-01-16-07.640234/inference_result/inference_result.jsonl
+
+# # ### gpt o1
+# python main.py --exp_config NPHARD_TSP_PIPELINE_MULTIPLE_RUNS --model_config OAI_O1_20241217_CONFIG --exp_logdir nphard_tsp_o1 --resume_from /home/vivineet/projects/evaluation/NPHardEval/TSP_02-07-2025/eureka-ml-insights/logs/NPHARD_TSP_PIPELINE_MULTIPLE_RUNS/nphard_tsp_o1/2025-02-01-23-26-17.167382/inference_result/inference_result.jsonl
+
+################################
+
+
+# ### gpt4o test
+
+# python main.py --exp_config NPHARD_TSP_PIPELINE_MULTIPLE_RUNS --model_config OAI_GPT4O_2024_11_20_CONFIG --exp_logdir nphard_tsp_level_gpt4o_2024-11-20 --resume_from /home/vivineet/projects/evaluation/NPHardEval/TSP_02-07-2025/eureka-ml-insights/logs/NPHARD_TSP_PIPELINE_MULTIPLE_RUNS/nphard_tsp_level_gpt4o_2024-11-20/2025-02-10-22-57-48.817329/inference_result/inference_result.jsonl
+
+################################
+
+
+
+# ### claude
+python main.py --exp_config NPHARD_TSP_PIPELINE_MULTIPLE_RUNS --model_config CLAUDE_3_5_SONNET_CONFIG --exp_logdir nphard_tsp_level_claude-3-5-sonnet-20241022 --resume_from /home/vivineet/projects/evaluation/NPHardEval/TSP_02-07-2025/eureka-ml-insights/logs/NPHARD_TSP_PIPELINE_MULTIPLE_RUNS/nphard_tsp_level_claude-3-5-sonnet-20241022/2025-02-01-23-25-15.059378/inference_result/inference_result.jsonl
+
+# ### gemini
+python main.py --exp_config NPHARD_TSP_PIPELINE_MULTIPLE_RUNS --model_config GEMINI_EXP_1206_CONFIG --exp_logdir nphard_tsp_level_gemini-exp-1206 --resume_from /home/vivineet/projects/evaluation/NPHardEval/TSP_02-07-2025/eureka-ml-insights/logs/NPHARD_TSP_PIPELINE_MULTIPLE_RUNS/nphard_tsp_level_gemini-exp-1206/2025-02-08-19-14-48.091758/inference_result/inference_result.jsonl
+
+### gpt4o
+python main.py --exp_config NPHARD_TSP_PIPELINE_MULTIPLE_RUNS --model_config OAI_GPT4O_2024_11_20_CONFIG --exp_logdir nphard_tsp_level_gpt4o_2024-11-20 --resume_from /home/vivineet/projects/evaluation/NPHardEval/TSP_02-07-2025/eureka-ml-insights/logs/NPHARD_TSP_PIPELINE_MULTIPLE_RUNS/nphard_tsp_level_gpt4o_2024-11-20/2025-02-01-23-25-46.681454/inference_result/inference_result.jsonl
+
+# # ### llama_3_1
+python main.py --exp_config NPHARD_TSP_PIPELINE_MULTIPLE_RUNS --model_config AIF_NT_LLAMA3_1_405B_INSTRUCT_EASTUS_OSS_CONFIG_2 --exp_logdir nphard_tsp_level_llama_3_1_2024-11-20 --resume_from /home/vivineet/projects/evaluation/NPHardEval/TSP_02-07-2025/eureka-ml-insights/logs/NPHARD_TSP_PIPELINE_MULTIPLE_RUNS/nphard_tsp_level_llama_3_1_2024-11-20/2025-02-01-23-28-15.277023/inference_result/inference_result.jsonl
+
+# # # ### phi4 ### not ready
+python main.py --exp_config NPHARD_TSP_PIPELINE_MULTIPLE_RUNS --model_config PHI4HF_CONFIG --exp_logdir nphard_tsp_level_phi4 --resume_from /home/vivineet/projects/evaluation/NPHardEval/TSP_02-07-2025/eureka-ml-insights/logs/NPHARD_TSP_PIPELINE_MULTIPLE_RUNS/nphard_tsp_level_phi4/2025-02-08-01-16-07.640234/inference_result/inference_result.jsonl
+
+
+# # ### gpt o1
+python main.py --exp_config NPHARD_TSP_PIPELINE_MULTIPLE_RUNS --model_config OAI_O1_20241217_CONFIG --exp_logdir nphard_tsp_o1 --resume_from /home/vivineet/projects/evaluation/NPHardEval/TSP_02-07-2025/eureka-ml-insights/logs/NPHARD_TSP_PIPELINE_MULTIPLE_RUNS/nphard_tsp_o1/2025-02-01-23-26-17.167382/inference_result/inference_result.jsonl
+
+
+
