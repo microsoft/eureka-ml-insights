9458a1a54e54a58b38e4da16f9480c2eba4cd93b
Besmira Nushi <benushi@microsoft.com>
2025-02-24 14:12:10 -0800

    address comments in data join

--- eureka_ml_insights/core/data_join.py
+++ eureka_ml_insights/core/data_join.py
@@ -28,9 +28,15 @@ class DataJoin(DataProcessing):
         super().__init__(data_reader_config, output_dir, output_data_columns)
         self.other_data_reader = other_data_reader_config.class_name(**other_data_reader_config.init_args)
         self.pandas_merge_args = pandas_merge_args
-
+        allowed_join_types = {"inner", "outer", "left", "right", "cross"}
+        join_type = self.pandas_merge_args["how"]
+        if join_type.lower() not in allowed_join_types:
+            raise ValueError(
+                f"Invalid join type '{join_type}'. Expected one of: {', '.join(allowed_join_types)}."
+            )
+        
     @classmethod
-    def from_config(cls, config):
+    def from_config(cls, config):        
         return cls(
             config.data_reader_config,
             config.output_dir,
@@ -44,8 +50,6 @@ class DataJoin(DataProcessing):
         other_df = self.other_data_reader.load_dataset()
         if len(df.columns) > 0 and len(other_df.columns) > 0:
             joined_df = pd.merge(df, other_df, **self.pandas_merge_args)
-            # joined_df = self.get_desired_columns(joined_df)
-            # self.write_output(joined_df)
         # handling corner cases when one of the data frames is empty
         elif len(df.columns) == 0:
             if (self.pandas_merge_args["how"] == 'inner'):
